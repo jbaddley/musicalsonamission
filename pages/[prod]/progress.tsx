d55b94def74c22c11b1aac5854c2d12106411eaa
Jason Baddley <jasonbaddley@gmail.com>
2022-12-25 19:19:55 -0700

    commit

--- pages/[prod]/progress.tsx
+++ pages/[prod]/progress.tsx
@@ -1,12 +1,13 @@
 import { useRouter } from "next/router";
+import { Container } from "semantic-ui-react";
 
 export default function Progress() {
   const router = useRouter();
   const { prod } = router.query;
 
   return (
-    <div>
+    <Container>
       <h2>{prod}</h2>
-    </div>
+    </Container>
   );
 }
