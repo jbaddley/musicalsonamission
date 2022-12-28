b04d2338b57ca2958b81aa43bc583bd812ce2562
Jason Baddley <jasonbaddley@gmail.com>
2022-12-27 22:13:10 -0700

    commit

--- pages/productions/[slug]/index.tsx
+++ pages/productions/[slug]/index.tsx
@@ -1,1 +1,37 @@
-export default function ViewProduction() {}
+import { Production } from "@prisma/client";
+import _ from "lodash";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useQuery } from "react-query";
+import { Layout } from "../../../components/Layout";
+import { ProductionsClientAPI, ProductionsServerAPI } from "../../../data";
+
+export default function ProductionView({ initialProduction }: { initialProduction: Production }) {
+  const router = useRouter();
+  const { slug } = router.query;
+  const { data: production = initialProduction } = useQuery(["production", slug], () =>
+    ProductionsClientAPI.getBySlug(String(slug))
+  );
+  return (
+    <Layout>
+      <Link href='/productions'>Back to Productions</Link>
+      <pre>{JSON.stringify(production)}</pre>
+    </Layout>
+  );
+}
+
+export async function getStaticPaths() {
+  const productions = await ProductionsServerAPI.getList();
+  return {
+    paths: productions.map(({ slug }) => ({ params: { slug } })),
+    fallback: false, // can also be true or 'blocking'
+  };
+}
+
+export async function getStaticProps({ params }: any) {
+  const initalProduction = await ProductionsServerAPI.getBySlug(params.slug);
+
+  return {
+    props: { initalProduction },
+  };
+}
